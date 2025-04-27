1c1451b9bbabd20b7314e01dd0c97af7f10471a0
Batuhan Erkul <baerkul@hotmail.com>
2025-04-27 11:54:03 +0300

    updated6

--- start.sh
+++ start.sh
@@ -1,28 +1,8 @@
 #!/bin/bash
 
-echo "📦 Bağımlılıklar yükleniyor..."
-apt-get update && apt-get install -y \
-    wget \
-    unzip \
-    curl \
-    gnupg \
-    fonts-liberation \
-    libasound2 \
-    libnss3 \
-    libxss1 \
-    libxtst6 \
-    libatk-bridge2.0-0 \
-    libgtk-3-0 \
-    libu2f-udev \
-    ca-certificates \
-    xdg-utils \
-    chromium \
-    chromium-driver \
-    python3-pip
-
-echo "🖥 Python paketleri kuruluyor..."
-pip3 install --upgrade pip
-pip3 install undetected-chromedriver selenium
+echo "🚀 Paketler kontrol ediliyor..."
+pip install --upgrade pip
+pip install -r requirements.txt
 
 echo "🚀 Bot başlatılıyor..."
 python3 e_bilet.py
